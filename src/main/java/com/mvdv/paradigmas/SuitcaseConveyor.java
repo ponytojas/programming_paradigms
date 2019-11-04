ac02f23432c0320094dfc3e95fe04f5845ad2257
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-11-04 19:48:50 +0100

    Refactor the code

--- src/main/java/com/mvdv/paradigmas/SuitcaseConveyor.java
+++ src/main/java/com/mvdv/paradigmas/SuitcaseConveyor.java
@@ -11,62 +11,67 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 
 public class SuitcaseConveyor {
-    private ArrayList <Suitcase> conveySuitcase;
+    private ArrayList<Suitcase> conveySuitcase;
     private Log log;
     private Lock lock = new ReentrantLock();
     private Condition full = lock.newCondition();
     private Condition empty = lock.newCondition();
-    
-    public SuitcaseConveyor(Log log){
-        this.conveySuitcase = new ArrayList <>();
+    private int suitcaseAmount = 80;
+
+    public SuitcaseConveyor(Log log) {
+        this.conveySuitcase = new ArrayList<>();
         this.log = log;
     }
 
-    public void depositSuitcase(Suitcase suitcase) throws InterruptedException{
-        
-        try{
+    public void depositSuitcase(Suitcase suitcase) throws InterruptedException {
+
+        try {
             this.lock.lock();
-            while(this.isConveyorFull()){
+
+            while (this.isConveyorFull()) {
                 full.await();
             }
-            if(!this.isConveyorFull()){
-                this.conveySuitcase.add(suitcase);
-                empty.signal();
-                
-                this.log.addConveyorEvent("Suitcase: " + suitcase.getSuitcaseID()
-                                            + " has been deposited");
-            }
-        }finally{
+            this.conveySuitcase.add(suitcase);
+            empty.signal();
+            this.log.addConveyorEvent("Suitcase: " + suitcase.getSuitcaseID() + " has been deposited");
+        } finally {
             this.lock.unlock();
         }
-        
+
     }
-    
-    public Suitcase getSuitcase() throws InterruptedException{
+
+    public int getSuitcaseAmount() {
+        return this.suitcaseAmount;
+    }
+
+    public Suitcase getSuitcase() throws InterruptedException {
         Suitcase toReturnSuitcase = null;
-        try{
+        try {
             this.lock.lock();
-            while(this.isConveyorEmpty()){
-                empty.await();
+            if (this.suitcaseAmount > 0) {
+
+                while (this.isConveyorEmpty()) {
+
+                }
+                toReturnSuitcase = this.conveySuitcase.remove(0);
+                if (!this.isConveyorFull())
+                    full.signal();
+
+                this.suitcaseAmount -= 1;
+                System.out.println("Cantidad de maletas: " + this.suitcaseAmount);
+
+                this.log.addConveyorEvent("Suitcase: " + toReturnSuitcase.getSuitcaseID() + " has been gated");
             }
-            if(!this.isConveyorFull()){
-                toReturnSuitcase =  this.conveySuitcase.remove(0);
-                full.signal();
-                
-                this.log.addConveyorEvent("Suitcase: " + 
-                                            toReturnSuitcase.getSuitcaseID() + 
-                                            " has been gated");
-            }
-        }finally{
+        } finally {
             this.lock.unlock();
         }
         return toReturnSuitcase;
     }
-    
-    public boolean isConveyorFull(){
+
+    public boolean isConveyorFull() {
         return (this.conveySuitcase.size() == 8);
     }
-    
+
     public boolean isConveyorEmpty() {
         return this.conveySuitcase.size() == 0;
     }
@@ -74,3 +79,5 @@ public class SuitcaseConveyor {
     public int getAllSize() {
         return this.conveySuitcase.size();
     }
+
+}
