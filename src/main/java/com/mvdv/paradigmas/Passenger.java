8ef6ea806d74199484bcc9fefa5322c4ac72e473
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-11-04 19:47:57 +0100

    Refactor the code

--- src/main/java/com/mvdv/paradigmas/Passenger.java
+++ src/main/java/com/mvdv/paradigmas/Passenger.java
@@ -8,15 +8,15 @@ import java.util.logging.Logger;
  *
  * @author Marcos Vicente && Daniel Villalobos
  */
-public class Passenger extends Thread{
-    
+public class Passenger extends Thread {
+
     private final String passengerID;
-    private ArrayList <Suitcase> passengerSuitcases;
+    private ArrayList<Suitcase> passengerSuitcases;
     private Log log;
     private SuitcaseConveyor suitcaseConveyor;
 
     public Passenger(int ID, Log log, SuitcaseConveyor suitcaseConveyor) {
-        this.passengerID = "Pasajero" +  String.valueOf(ID);
+        this.passengerID = "Pasajero" + String.valueOf(ID);
         this.passengerSuitcases = new ArrayList<>();
         this.passengerSuitcases.add(new Suitcase(ID, 1));
         this.passengerSuitcases.add(new Suitcase(ID, 2));
@@ -27,30 +27,32 @@ public class Passenger extends Thread{
     public String getPassengerID() {
         return passengerID;
     }
-    
-    public boolean hasSuitcase(){
+
+    public boolean hasSuitcase() {
         return this.passengerSuitcases.isEmpty();
     }
-    
+
     @Override
-    public void run(){
-        while(!this.passengerSuitcases.isEmpty()){
+    public void run() {
+        while (!this.passengerSuitcases.isEmpty()) {
             try {
-                this.suitcaseConveyor.
-                        depositSuitcase(this.passengerSuitcases.remove(0));
+                this.suitcaseConveyor.depositSuitcase(this.passengerSuitcases.remove(0));
             } catch (InterruptedException ex) {
-                Logger.getLogger(Passenger.class.getName()).
-                        log(Level.SEVERE, null, ex);
+                Logger.getLogger(Passenger.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            if(this.passengerSuitcases.isEmpty())
-                this.log.addPassengerEvent("No more suitcases for passenger " 
-                                            + this.passengerID);
+            try {
+                Thread.sleep((int) (Math.random() * ((1000 - 500) + 1)) + 500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Passenger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            if (this.passengerSuitcases.isEmpty())
+                this.log.addPassengerEvent("No more suitcases for passenger " + this.passengerID);
             else
-                this.log.addPassengerEvent("Still one suitcase for passenger " 
-                                            + this.passengerID);
+                this.log.addPassengerEvent("Still one suitcase for passenger " + this.passengerID);
         }
-        
+
         System.out.println("See you later Aligator: " + this.passengerID);
     }
 }
