441496af17c3f0ffb7350b3d47120804898d372a
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-12-03 15:52:34 +0100

    Starting to the new log system
    For now, check if lock file exists, create log and lock, and start to write the file

--- src/main/java/com/mvdv/paradigmas/Log.java
+++ src/main/java/com/mvdv/paradigmas/Log.java
@@ -5,12 +5,16 @@
  */
 package com.mvdv.paradigmas;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.util.Date;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  *
@@ -18,27 +22,84 @@ import java.sql.Timestamp;
  */
 public class Log {
     private final String currentDirectory = System.getProperty("user.dir");
+    private File log = new File(currentDirectory + "/airport.log");
+    BufferedWriter writer = new BufferedWriter(new FileWriter(currentDirectory + "/airport.log", true));
 
     public Log() throws IOException {
-        File tmpDir = new File(this.currentDirectory + "/airport.log.lock");
-        boolean exists = tmpDir.exists();
-        System.out.println("En el directorio " + currentDirectory + ", existe un fichero airport.lock: " + exists);
-        if (!exists)
-            this.createLockFile();
-        else{
-            System.out.println("The program did not finish correctly in the last run");
-            System.out.println("Creating error backup");
-            File errorCopyFile = new File(this.currentDirectory + "/error-airport.log.lock");
-            
+        if (!log.exists())
+            this.createFile(false);
+        else {
+            File tmpDir = new File(this.currentDirectory + "/airport.log.lock");
+            boolean exists = tmpDir.exists();
+            System.out.println("En el directorio " + currentDirectory + ", existe un fichero airport.lock: " + exists);
+            if (!exists)
+                this.createFile(true);
+            else {
+                System.out.println("The program did not finish correctly in the last run");
+                System.out.println("Creating error backup");
+                File error = new File(this.currentDirectory + "/error-airport.log");
+                InputStream is = null;
+                OutputStream os = null;
+                try {
+                    is = new FileInputStream(this.log);
+                    os = new FileOutputStream(error);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = is.read(buffer)) > 0) {
+                        os.write(buffer, 0, length);
+                    }
+                } finally {
+                    is.close();
+                    is = null;
+                    os.close();
+                    os = null;
+                    error = null;
+                    System.gc();
+                }
+            }
         }
     }
 
-    private void createLockFile() {
+    private void createFile(boolean isLock) {
+        if (isLock) {
+            try {
+                File file = new File(this.currentDirectory + "/airport.log.lock");
+                file.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                File fileLock = new File(this.currentDirectory + "/airport.log.lock");
+                File fileLog = new File(this.currentDirectory + "/airport.log");
+                fileLock.createNewFile();
+                fileLog.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void writeToTheLog(String inputText, String inputType) {
+        Date date = new Date();
+        Timestamp ts = new Timestamp(date.getTime());
+        switch (inputType) {
+        case "Info":
+            inputText = ts + "  I/ " + inputText;
+            break;
+        case "Error":
+            inputText = ts + "  E/ " + inputText;
+            break;
+        default:
+            inputText = ts + "  W/ " + inputText;
+            break;
+        }
+        System.out.println(inputText);
         try {
-            File file = new File(this.currentDirectory + "/airport.log.lock");
-            file.createNewFile();
+            this.writer.append(inputText);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
