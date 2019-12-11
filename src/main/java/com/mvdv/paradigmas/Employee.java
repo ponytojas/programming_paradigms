62f6fa4c47a8ab65125f1b87d4151e340fbc985d
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-12-11 14:40:12 +0100

    The employee needs to write in the interface

--- src/main/java/com/mvdv/paradigmas/Employee.java
+++ src/main/java/com/mvdv/paradigmas/Employee.java
@@ -19,14 +19,16 @@ public class Employee extends Thread {
     private Airplane airplane;
     private Log log;
     private Stopper stopper;
+    private Interface gui;
 
-    public Employee(int id, SuitcaseConveyor suitcaseConveyor, Airplane airplane, Log log, Stopper stopper) {
+    public Employee(int id, SuitcaseConveyor suitcaseConveyor, Airplane airplane, Log log, Stopper stopper, Interface gui) {
         this.id = id;
         this.suitcases = new ArrayList<>();
         this.suitcaseConveyor = suitcaseConveyor;
         this.airplane = airplane;
         this.log = log;
         this.stopper = stopper;
+        this.gui = gui;
     }
 
     @Override
@@ -37,6 +39,8 @@ public class Employee extends Thread {
                 this.stopper.checkEmployee(this.id);
                 
                 this.suitcases.add(this.suitcaseConveyor.getSuitcase());
+                this.gui.updateEmployeeText(this.id, true, this.suitcases.get(0).getSuitcaseID());
+                this.gui.updateEmployeeGoBack(this.id, true);
                 String textForLog = "The employee " + this.id + " has get a suitcase";
                 System.out.println(textForLog);
                 this.log.writeToTheLog(textForLog, "Info");
@@ -50,7 +54,11 @@ public class Employee extends Thread {
                 this.stopper.checkGlobal();
                 this.stopper.checkEmployee(this.id);
                 
+                //this.gui.addContentToAirplane(this.suitcases.get(0).getSuitcaseID());
                 this.airplane.setSuitcase(this.suitcases.remove(0));
+                
+                this.gui.updateEmployeeGoBack(this.id, false);
+                this.gui.updateEmployeeText(this.id, false, "");
                 String textForOtherLog = "The employee " + this.id + " has deposit a suitcase";
                 System.out.println(textForOtherLog);
                 this.log.writeToTheLog(textForOtherLog, "Info");
