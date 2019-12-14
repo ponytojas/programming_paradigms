67e34af38643ef8fdae7dabf1e3a816f1542fe1d
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-12-14 18:48:34 +0100

    We have delegate all logic to a new class

---
+++ src/main/java/com/mvdv/paradigmas/client/Connection.java
@@ -0,0 +1,53 @@
+package com.mvdv.paradigmas.client;
+
+import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public class Connection extends Thread {
+    private ArrayList <String> airplaneContent;
+    private ArrayList <String> conveyorConten;
+    private ClientInterface clientInterface;
+
+    public Connection(ClientInterface clientInterface){
+        this.clientInterface = clientInterface;
+    }
+
+    @Override
+    public void run() {
+        Interface objGetInfo = null;
+        airplaneContent = new ArrayList<>();
+
+        while (true) {
+            try {
+                objGetInfo = (Interface) Naming.lookup("//127.0.0.1/objGetInfo");
+            } catch (MalformedURLException | RemoteException | NotBoundException e1) {
+            }
+
+            if (objGetInfo != null) {
+                try {
+                    ArrayList<String> tempAirplane = objGetInfo.getAirplaneIDs();
+                    // ArrayList<String> tempConveyor = objGetInfo.getConveyorIDs();
+
+                    if (!airplaneContent.equals(tempAirplane)) {
+                        System.out.println("Content changed");
+                        tempAirplane.removeAll(airplaneContent);
+                        airplaneContent.addAll(tempAirplane);
+                        
+                       this.clientInterface.updateAirplaneContentText(airplaneContent);
+
+                        tempAirplane = null;
+
+                    } else {
+                        System.out.println("There's no change");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error" + e.getMessage());
+                }
+            }
+        }
+    }
+}
