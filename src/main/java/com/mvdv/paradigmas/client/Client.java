efbad2314399536f763644b21352c64298405f03
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2019-12-14 17:00:22 +0100

    Compare old Array and new and only show the newest data

--- src/main/java/com/mvdv/paradigmas/client/Client.java
+++ src/main/java/com/mvdv/paradigmas/client/Client.java
@@ -4,50 +4,52 @@
  * and open the template in the editor.
  */
 package com.mvdv.paradigmas.client;
-import java.rmi.Naming;
-import java.util.ArrayList;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 /**
  *
  * @author marks
  */
 public class Client extends Thread {
-        
-    public static void main(String[] args){
-        while (true){
-        try{
-            
-        
-            Interface objGetInfo = (Interface) Naming.lookup("//127.0.0.1/objGetInfo");
-         
-            
-            
-            ArrayList<String> arrayAirplane = objGetInfo.getAirplaneIDs();
-            ArrayList<String> arrayConveyor = objGetInfo.getConveyorIDs();
-            //this.soutAirplane(arrayAirplane);
-            for(int i= 0; i<arrayAirplane.size(); i++){
-              System.out.println(arrayAirplane.get(i));
-            }
-          
-          }
-           // Aqui codigo de la interfaz
-            
-            
 
-        
-        catch (Exception e){
-            System.out.println("Error" + e.getMessage());
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayAirplane = new ArrayList<>();
+        ArrayList<String> arrayConveyor = new ArrayList<>();
+        Interface objGetInfo = null;
+
+        while (true) {
+            try {
+                objGetInfo = (Interface) Naming.lookup("//127.0.0.1/objGetInfo");
+            } catch (MalformedURLException | RemoteException | NotBoundException e1) {
+                e1.printStackTrace();
+            }
+            try {
+                ArrayList<String> tempAirplane = objGetInfo.getAirplaneIDs();
+                ArrayList<String> tempConveyor = objGetInfo.getConveyorIDs();
+
+                if (!arrayAirplane.equals(tempAirplane)) {
+                    System.out.println("Ha cambiado en este contenido: ");
+                    tempAirplane.removeAll(arrayAirplane);
+
+                    for (String airplaneStuff : tempAirplane)
+                        System.out.println(airplaneStuff);
+
+                    arrayAirplane = tempAirplane;
+                    tempAirplane = null;
+                    
+                } else {
+                    System.out.println("No ha cambiado nada");
+                }
+                Thread.sleep(100);
+            } catch (Exception e) {
+                System.out.println("Error" + e.getMessage());
+            }
         }
     }
-        
-    }  
-        //NO SE PORQUE NO VA ESTO:
-        //private void soutAirplane(ArrayList<String> arrayAirplane){
-            //for(int i= 0; i<arrayAirplane.size(); i++){
-              //System.out.println(arrayAirplane.get(i));
-        //}
-    }
-//}
-    
-    
+}
